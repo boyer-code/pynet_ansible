a9b3976b5374e7793b59747b59acca589674525d
Joey Boyer <joey@networkbit.com>
2016-04-07 21:11:10 -0700

    Update 1_10.py

--- exercise_1_10.py
+++ exercise_1_10.py
@@ -1,15 +1,21 @@
 #!/usr/bin/env python
 
-#9 Find all of the crypto map entries that are using PFS group2
+#10. Using ciscoconfparse find the crypto maps that are not using AES (based-on the transform set name). Print these entries and their corresponding transform set name.
 
 from ciscoconfparse import CiscoConfParse
 
 cisco_cfg = CiscoConfParse("cisco_crypto.txt")
 #crypto_cfg = cisco_cfg.find_objects(r"^crypto map CRYPTO")
 #cisco_cfg.find_objects_w_child(parentspec=r"^interface", childspec=r"no ip address")
-group_2_crypto = cisco_cfg.find_objects_w_child(parentspec=r"^crypto map CRYPTO", childspec=r"group2")
+#cisco_cfg.find_objects_wo_child(parentspec=r"^interface", childspec=r"no ip address")
+#group_2_crypto = cisco_cfg.find_objects_w_child(parentspec=r"^crypto map CRYPTO", childspec=r"group2")
+no_aes = cisco_cfg.find_objects_wo_child(parentspec=r"^crypto map CRYPTO", childspec=r"AES")
 
-print "\nThese Crypto Maps use PFS group 2"
-for line in group_2_crypto:
+
+print "\nThese crypto maps aren't using AES in their transform-set:"
+for line in no_aes:
     print line.text
+    for kids in line.children:
+            if "transform" in kids.text:
+                    print kids.text
 
